c06981d6053d7063823aeccff6f64bb844e95a90
suri312006 <suri312006@gmail.com>
2024-03-01 18:52:40 -0800

    did some interesting cos_similarity stuff

--- embeddings.py
+++ embeddings.py
@@ -7,6 +7,7 @@ model = AutoModel.from_pretrained("microsoft/codebert-base")
 
 # pad it with 0's
 # pad it with 1's
+# does this change the difference?
 
 
 def pl_embedding(pl: str, nl_tokens):
@@ -23,24 +24,31 @@ def pl_embedding(pl: str, nl_tokens):
 
 
 def padded_0_cos_similarity(vec1: torch.Tensor, vec2: torch.Tensor) -> float:
-    cos = torch.nn.CosineSimilarity(dim=2)
-    # print(vec1.shape - vec2.shape)
+    cos = torch.nn.CosineSimilarity(dim=1)
+    thing = torch.split(vec1,split_size_or_sections=768,  dim=2) 
+    
+    print(thing)
+    print(thing[0].shape)
+    
     # Get the dimensions of the tensors
     dim_vec1 = vec1.shape
     dim_vec2 = vec2.shape
+    print(dim_vec2)
+    print(dim_vec1)
 
-# Perform subtraction of dimensions
+    # Perform subtraction of dimensions
     difference = [dim1 - dim2 for dim1, dim2 in zip(dim_vec1, dim_vec2)]
-    if difference[1] < 0:
-        vec1 = F.pad(input=vec1, pad=(
-            0, 0, -1*difference[1], 0), mode='constant', value=0)
 
-    elif difference[1] > 0:
-        vec2 = F.pad(input=vec2, pad=(
-            0, 0, difference[1], 0), mode='constant', value=0)
-
-    else:
-        None
+    # if difference[1] < 0:
+    #     vec1 = F.pad(input=vec1, pad=(
+    #         0, 0, -1*difference[1], 0), mode='constant', value=0)
+    #
+    # elif difference[1] > 0:
+    #     vec2 = F.pad(input=vec2, pad=(
+    #         0, 0, difference[1], 0), mode='constant', value=0)
+    #
+    # else:
+    #     None
 
     cos_vector = cos(vec1, vec2)
     return torch.mean(cos_vector)
@@ -49,6 +57,9 @@ def padded_0_cos_similarity(vec1: torch.Tensor, vec2: torch.Tensor) -> float:
 
 natural_language = "return larger number"
 nl_tokens = tokenizer.tokenize(natural_language)
+
+gibberish_natural_language = "fai;uhdlahdlawhdiwaulhdiauhdia"
+gibb_nl_tokens = tokenizer.tokenize(natural_language)
 
 code_1 = """
 def max(a,b):
@@ -66,20 +77,26 @@ code_3 = "edyuasefghliahefklashfjk,sdhvjfdhgkjghxdjkhgfsjgjyypkhsdgjh"
 code_4 = "flyuraglkjfhg arsjkfhvaskbrhgskrjghvjkaernghvagvhkashgnvka    "
 
 embeds_1 = pl_embedding(code_1, nl_tokens)
+embeds_12 = pl_embedding(code_1, gibb_nl_tokens)
 embeds_2 = pl_embedding(code_2, nl_tokens)
 embeds_3 = pl_embedding(code_3, nl_tokens)
 embeds_4 = pl_embedding(code_4, nl_tokens)
-# print(embeds_1)
-# print(embeds_2)
-# print(embeds_1.size())
-# print(embeds_2.size())
+print(embeds_1)
+print(embeds_2)
+print(embeds_1.size())
+print(embeds_2.size())
 c1 = padded_0_cos_similarity(embeds_1, embeds_2)
 c2 = padded_0_cos_similarity(embeds_1, embeds_3)
 c3 = padded_0_cos_similarity(embeds_3, embeds_4)
 c4 = padded_0_cos_similarity(embeds_1, embeds_1)
+# c12 = padded_0_cos_similarity(embeds_1, embeds_12)
 
 
-print(f"cosine similarity between {code_1} and {code_2}: {c1} ")
-print(f"cosine similarity between {code_1} and {code_3}: {c2} ")
-print(f"cosine similarity between {code_3} and {code_4}: {c3} ")
-print(f"cosine similarity between {code_1} and {code_1}: {c4} ")
+print(
+    f"cosine similarity between {code_1} and {code_1} \n with {gibberish_natural_language} \n cosine similarity = {c12} ")
+
+
+# print(f"cosine similarity between {code_1} and {code_2}: {c1} ")
+# print(f"cosine similarity between {code_1} and {code_3}: {c2} ")
+# print(f"cosine similarity between {code_3} and {code_4}: {c3} ")
+# print(f"cosine similarity between {code_1} and {code_1}: {c4} ")
