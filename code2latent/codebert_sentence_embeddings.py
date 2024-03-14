112ebad13ae33db70be1a99fdfd79f475ba0b0d6
suri312006 <suri312006@gmail.com>
2024-03-14 11:25:51 -0700

    organizing

---
+++ code2latent/codebert_sentence_embeddings.py
@@ -0,0 +1,12 @@
+
+from sentence_transformers import SentenceTransformer
+from example_functions import functions
+code_fragments = functions
+
+model_2 = SentenceTransformer('mchochlov/codebert-base-cd-ft')
+
+embeddings = model_2.encode(code_fragments)
+
+
+
+print(embeddings)
--- code2latent/codet5_embeddings.py
+++ code2latent/codet5_embeddings.py
@@ -9,5 +9,8 @@ model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True).to(device)
 inputs = tokenizer.encode("def print_hello_world():\tprint('Hello World!')", return_tensors="pt").to(device)
 embedding = model(inputs)[0]
 print(f'Dimension of the embedding: {embedding.size()[0]}, with norm={embedding.norm().item()}')
+
+#TODO: check if the dims are always constant
+
 # Dimension of the embedding: 256, with norm=1.0
 print(embedding)
