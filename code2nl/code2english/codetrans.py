9bb8bdd6ac331c50ebe83b6c16942804d948ae10
suri312006 <suri312006@gmail.com>
2024-03-14 21:19:05 -0700

    looking into english2latentspace models

---
+++ code2nl/code2english/codetrans.py
@@ -0,0 +1,11 @@
+#https://huggingface.co/SEBIS/code_trans_t5_base_source_code_summarization_python
+from transformers import AutoTokenizer, AutoModelWithLMHead, SummarizationPipeline
+
+pipeline = SummarizationPipeline(
+    model=AutoModelWithLMHead.from_pretrained("SEBIS/code_trans_t5_base_source_code_summarization_python"),
+    tokenizer=AutoTokenizer.from_pretrained("SEBIS/code_trans_t5_base_source_code_summarization_python", skip_special_tokens=True),
+    device=0
+)
+
+tokenized_code = '''with open ( CODE_STRING , CODE_STRING ) as in_file : buf = in_file . readlines ( )  with open ( CODE_STRING , CODE_STRING ) as out_file : for line in buf :          if line ==   " ; Include this text   " :              line = line +   " Include below  "          out_file . write ( line ) '''
+pipeline([tokenized_code])
