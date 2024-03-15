1a768cf47091baf4948ea12bb71980102e69a64f
suri312006 <suri312006@gmail.com>
2024-03-15 11:59:00 -0700

    djawoijda

--- rag_comparison.py
+++ rag_comparison.py
@@ -1,8 +1,14 @@
-import code2nl
-import code2latent
+# from . import code2nl
+# from . import code2latent
+
+from code2nl import rag as code2nl_rag
+from code2latent import rag as code2lat_rag
+
 from random_functions import test_functions
 
 
+code2nl_rag.get_n_highest_similar_to(
+    "heakdjwajdaw", test_functions, 5, "codet5p")
+code2lat_rag.get_n_highest_similar_to(
+    "heakdjwajdaw", test_functions, 5, "codet5p")
 
-code2nl.rag.get_n_highest_similar_to("heakdjwajdaw", test_functions, 5, "codet5p")
-code2nl.rag.get_n_highest_similar_to("heakdjwajdaw", test_functions, 5, "codet5p")
