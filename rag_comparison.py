9e1fba3bc1014744b6183a6267b5fca054b18673
suri312006 <suri312006@gmail.com>
2024-03-15 17:40:20 -0700

    rand

--- rag_comparison.py
+++ rag_comparison.py
@@ -10,7 +10,7 @@ query = """
   take a list and return the number of items in that list that are greater than 0  
 """
 
-code2nl_rag.get_n_highest_similar_to(query, test_functions, 5, "gemma")
+code2nl_rag.get_n_highest_similar_to(query, test_functions, 5, c2e_model_name="gemma", e2l_model_name="jina")
 
 # TODO: do this
 # code2lat_rag.get_n_highest_similar_to(
